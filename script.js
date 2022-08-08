71bd05238e76325a9173422813915e5e2b8a7c16
tracey <tracey.d.huang@gmail.com>
2022-08-08 22:20:19 +1000

    Fix read option to be responsive to input

--- script.js
+++ script.js
@@ -10,11 +10,8 @@ function Book(title, author, pages, read) {
 }
 
 //Add methods to prototype object 
-Book.prototype.info = function(read) {
-    let print = 'not read yet';
-    if (read) {
-        print = 'read';
-    }
+Book.prototype.info = function() {
+    this.read ? print = 'read' : print = 'not read yet'; 
     return `${this.title} by ${this.author}, ${this.pages} pages, ${print}`
 }
 
@@ -25,7 +22,7 @@ const pride = new Book('Pride and Prejudice', 'Jane Austen', 467, true);
 const killBird = new Book('To kill a Mockingbird', 'Harper Lee', 987, false);
 
 
-console.log(hungerGames.info());
+console.log(pride.info());
 
 //ask user for input and add book to library 
 function addBookToLibrary() {
