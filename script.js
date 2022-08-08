611a5e6bc82f5b3b7d8f79026548fece6c68f6d4
tracey <tracey.d.huang@gmail.com>
2022-08-08 22:13:47 +1000

    Add sample book in javascript and test if info in functioning

--- script.js
+++ script.js
@@ -2,21 +2,25 @@
 let myLibrary = []; 
 
 //book constructor function 
-function books(title, author, pages, read) {
+function Book(title, author, pages, read) {
     this.title = title; 
     this.author = author; 
     this.pages = pages; 
     this.read = read; 
-    this.info = function () {
-        let print = 'not read yet';
-        if (read) {
-            print = 'read';
-        }
-        return `${title} by ${author}, ${pages} pages, ${print}`
+}
+
+Book.prototype.info = function(read) {
+    let print = 'not read yet';
+    if (read) {
+        print = 'read';
     }
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${print}`
 }
 
 //create new book 
+const hungerGames = new Book('The Hunger Games', 'Suzanne Collins', 300, true);
+
+console.log(hungerGames.info());
 
 //ask user for input and add book to library 
 function addBookToLibrary() {
