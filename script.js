ddddad33df1afda5f7dc1893fa21a0ffe000c99e
tracey <tracey.d.huang@gmail.com>
2022-08-13 01:28:33 +1000

    Create book function

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 //Library array stores all book object 
 let myLibrary = []; 
 
+//Select interactive elements on page 
+const submitFormButton = document.querySelector('#submit');
+const removeBookButton = document.querySelector('#remove');
+
 //constructor function that makes new book objects 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title; 
     this.author = author; 
     this.pages = pages; 
+    this.read = false ; 
 }
+
+function createBookCard (book) {
+    const bookCard = document.createElement('div');
+    const removeCard = document.createElement('button'); 
+    const title = document.createElement('div'); 
+    const author = document.createElement('div'); 
+    const page = document.createElement('div'); 
+    
+    bookCard.classList.add('book');
+    removeCard.classList.add('delete');
+    title.classList.add('title');
+
+    title.textContent = `${book.title}`;
+    author.textContent = `By ${author.title}`;
+    page.textContent = `${page.title} pages`;
+
+}
+
 
 const testBook = new Book('Title', "someone", 50);
 
-Book.prototype.test = function () {
-    console.log(this.title + this.author + this.pages);
+
+//Methods for book object 
+//add book to main container 
+Book.prototype.add = function () {
+    console.log('add' + this.title);
+}
+
+//remove book from main container 
+Book.prototype.remove = function () {
+    console.log('remove' + this.title);
 }
 
 //function that can take user's input and store the new book objects into an array 
+//function addBookToLibrary() {}
 
-
-
+//function that loops through array and displays each book on page 
+//myLibrary.forEach(); 
 
 //Elements of book object that will be injected into the DOM 
 
