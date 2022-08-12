19081416cdfb441a6674602367bb12ad8c8ce208
tracey <tracey.d.huang@gmail.com>
2022-08-13 04:00:53 +1000

    Format code

--- script.js
+++ script.js
@@ -1,52 +1,79 @@
 //Library array stores all book object 
 let myLibrary = []; 
 
-//Select interactive elements on page 
-const submitFormButton = document.querySelector('#submit');
-const removeBookButton = document.querySelector('#remove');
-
 //constructor function that makes new book objects 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     this.title = title; 
     this.author = author; 
     this.pages = pages; 
-    this.read = false ; 
 }
 
-function createBookCard (book) {
-    const bookCard = document.createElement('div');
-    const removeCard = document.createElement('button'); 
-    const title = document.createElement('div'); 
-    const author = document.createElement('div'); 
-    const page = document.createElement('div'); 
-    
-    bookCard.classList.add('book');
-    removeCard.classList.add('delete');
-    title.classList.add('title');
-
-    title.textContent = `${book.title}`;
-    author.textContent = `By ${author.title}`;
-    page.textContent = `${page.title} pages`;
-
-}
-
-
-const testBook = new Book('Title', "someone", 50);
-
-
-//Methods for book object 
-//add book to main container 
-Book.prototype.add = function () {
-    console.log('add' + this.title);
-}
-
-//remove book from main container 
+//Method:remove book from main container 
 Book.prototype.remove = function () {
     console.log('remove' + this.title);
 }
 
+//Select buttons elements on page 
+const submitFormButton = document.querySelector('#submit');
+const removeBookButton = document.querySelector('#remove');
+
+//input form 
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const resetForm = document.querySelector('form');
+
+//Buttons 
+submitFormButton.addEventListener('click', addBookToLibrary);
+
 //function that can take user's input and store the new book objects into an array 
-//function addBookToLibrary() {}
+function addBookToLibrary() {
+    myLibrary.push(new Book(title.value, author.value, pages.value));
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(document.querySelector('[data-number="i"]'));
+        if (document.querySelector('[data-number="i"]') == null) {
+        createBookCard(myLibrary[i], i);
+    }
+    }
+    console.log(myLibrary);
+}
+
+
+
+//check if card element exists 
+function exists (arrayItem) {
+
+}
+
+
+function createBookCard(book, i) {
+
+    const bookShelf = document.querySelector('#bookshelf');
+    const bookCard = document.createElement('div');
+    const removeCard = document.createElement('button'); 
+    const title = document.createElement('div'); 
+    const author = document.createElement('div'); 
+    const pages = document.createElement('div'); 
+    
+    bookCard.classList.add('book');
+    bookCard.dataset.number = `${i}`;
+    bookCard.setAttribute('id', `num-${i}`);
+    removeCard.classList.add('delete');
+    title.classList.add('title');
+
+    title.textContent = `${book.title}`;
+    author.textContent = `By ${book.author}`;
+    pages.textContent = `${book.pages} pages`;
+
+    bookCard.append(removeCard);
+    bookCard.append(title);
+    bookCard.append(author);
+    bookCard.append(pages);
+    bookShelf.append(bookCard);
+
+    console.log(bookCard);
+
+}
 
 //function that loops through array and displays each book on page 
 //myLibrary.forEach(); 
@@ -57,7 +84,8 @@ Book.prototype.remove = function () {
 //Add book object into myLibrary array 
 //Create book object
 
-
+////////////////////////test//////////////////////
+let test = new Book('Title', "someone", 50);
 ////////////////////////Modal//////////////////////
 
 //Select buttons that open and close modal 
